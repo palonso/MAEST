83965b9485cdd561348c5db964b4426f46feaea9
palonso <pablo.alonso@upf.edu>
2023-07-13 18:20:29 +0200

    Remove test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from essentia.standard import MonoLoader, TensorflowPredictEffnetDiscogs
-
-audio = MonoLoader(filename="/home/palonso/rock.mp3", sampleRate=16000, resampleQuality=4)()
-model = TensorflowPredictEffnetDiscogs(graphFilename="/media/data/models/essentia-models/feature-extractors/discogs-effnet/discogs-effnet-bs64-1.pb", output="PartitionedCall:1")
-embeddings = model(audio)
-
