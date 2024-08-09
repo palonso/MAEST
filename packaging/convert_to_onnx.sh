6dc822194cafa26175784fd5ba4cf69c4df6efc1
palonso <pablo.alonso@upf.edu>
2024-08-09 17:49:26 +0200

    Add packaging code

---
+++ packaging/convert_to_onnx.sh
@@ -0,0 +1,34 @@
+# set -e
+
+for model in \
+    discogs-maest-30s-pw-129e-519l \
+    # discogs-maest-10s-fs-129e \
+    # discogs-maest-10s-dw-75e \
+    # discogs-maest-5s-pw-129e \
+    # discogs-maest-10s-pw-129e \
+    # discogs-maest-20s-pw-129e \
+    # discogs-maest-30s-pw-129e \
+    # discogs-maest-30s-pw-73e-ts
+do
+    echo processing "$model"
+
+    git clone git@hf.co:mtg-upf/"$model" input_models/
+
+    cd input_models/"$model"
+
+    git remote set-url origin https://palonso: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@huggingface.co/mtg-upf/"$model"
+
+    git lfs pull
+
+    cd ../../
+
+    python safetensors_to_pytorch.py --base_path input_models/"$model"
+
+
+    optimum-cli export onnx \
+        --model input_models/"$model" \
+        --task audio-classification \
+        output_models/ 
+
+    mv output_models/model.onnx output_models/"$model".onnx
+done
