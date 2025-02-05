cb8501ef51186a5d0beec74508f2f5972b11facf
Pablo Alonso <pablo.alonso@upf.edu>
2025-02-05 15:33:07 +0100

    Simplify melspectrogram extractor
    Use existing TensorflowInputMusiCNN algo instead of reimplementing it
    from basic algorithms.

--- models/helpers/melspectrogram_extractor.py
+++ models/helpers/melspectrogram_extractor.py
@@ -3,59 +3,29 @@ from essentia.streaming import (
     VectorInput,
     MonoLoader,
     FrameCutter,
-    Windowing,
-    Spectrum,
-    MelBands,
-    UnaryOperator,
+    TensorflowInputMusiCNN,
 )
 from essentia import Pool, run, reset
 
 
 class MelSpectrogramExtractor:
+    # mel-spec signature params
     sample_rate = 16000
     resample_quality = 4
-    hop_size = 256
     frame_size = 512
-    n_mels = 96
-    window_type = "hann"
-    low_frequency_bound = 0
-    high_frequency_bound = sample_rate / 2
-    warping_formula = "slaneyMel"
-    weighting = "linear"
-    normalize = "unit_tri"
-    bands_type = "power"
+    hop_size = 256
 
     def __init__(self):
         self.pool = Pool()
-        self.frameCutter = FrameCutter(
+        self.frame_cutter = FrameCutter(
             frameSize=self.frame_size,
             hopSize=self.hop_size,
             silentFrames="keep",
         )
-        self.windowing = Windowing(
-            type=self.window_type,
-            normalized=False,
-        )
-        self.spec = Spectrum(size=self.frame_size)
-        self.mels = MelBands(
-            inputSize=self.frame_size // 2 + 1,
-            numberBands=self.n_mels,
-            sampleRate=self.sample_rate,
-            lowFrequencyBound=self.low_frequency_bound,
-            highFrequencyBound=self.high_frequency_bound,
-            warpingFormula=self.warping_formula,
-            weighting=self.weighting,
-            normalize=self.normalize,
-            type=self.bands_type,
-            log=False,
-        )
+        self.melspec = TensorflowInputMusiCNN()
 
-        self.shift = UnaryOperator(type="identity", scale=1e4, shift=1)
-        self.compressor = UnaryOperator(type="log10")
-
-        self.frameCutter.frame >> self.windowing.frame >> self.spec.frame
-        self.spec.spectrum >> self.mels.spectrum
-        self.mels.bands >> self.shift.array >> self.compressor.array >> (self.pool, 'mel_bands')
+        self.frame_cutter.frame >> self.melspec.frame
+        self.melspec.bands >> (self.pool, "mel_bands")
 
     def __call__(self, audio):
         # make sure audio is float32 for Essentia
@@ -63,18 +33,19 @@ class MelSpectrogramExtractor:
 
         # set vector input and connect the network
         vector_input = VectorInput(audio)
-        vector_input.data >> self.frameCutter.signal
+        vector_input.data >> self.frame_cutter.signal
 
+        # run the network and get the mel bands
         run(vector_input)
         mel_bands = np.array(self.pool["mel_bands"])
 
-        self.pool.clear()
+        # clear the pool and reset the vector input
         reset(vector_input)
-
-        vector_input.data.disconnect(self.frameCutter.signal)
+        vector_input.data.disconnect(self.frame_cutter.signal)
+        self.pool.clear()
         del vector_input
 
-        # to freq, time
+        # return freq, time
         return mel_bands.T
 
     def load_audio(self, audio_file):
